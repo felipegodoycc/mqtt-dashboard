a64ed0b9862b6bb3055fefefb82182883c3829db
Felipe Godoy <felipe.godoy@ceinf.cl>
2019-12-10 18:58:17 -0300

    update component

--- src/app/modules/home/dashboard/dashboard.component.ts
+++ src/app/modules/home/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { MedicionService } from 'src/app/core/services/medicion.service';
 import { MedicionDTO } from 'src/app/shared/dto/medicion.dto';
 import { Medicion } from 'src/app/shared/models/medicion.model';
+import { DataLineChart } from '../line-chart/interface/data.interface';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,24 +13,85 @@ import { Medicion } from 'src/app/shared/models/medicion.model';
 })
 export class DashboardComponent implements OnInit {
   mediciones: Medicion[] = [];
-  desde = '2019-11-17 19:38:11.553-03:00';
-  hasta = '2019-11-17 19:40:11.553-03:00';
-  fecha: Date;
+  graphData: DataLineChart;
+  isData = false;
+  topics = [
+    { value: 'casa/pieza/temp', viewValue: 'casa/pieza/temp'},
+    { value: 'casa/pieza/hum', viewValue: 'casa/pieza/hum'},
+    { value: 'casa/patio/temp', viewValue: 'casa/patio/temp'},
+    { value: 'casa/patio/hum', viewValue: 'casa/patio/hum'},
+  ]
+  selected = this.topics[0].value
+  dateSelected = false;
+  desde: string;
+  hasta: string;
 
-  constructor(private breakpointObserver: BreakpointObserver,
-              public medicionService: MedicionService) {}
+  constructor(public medicionService: MedicionService) {
+              }
 
   ngOnInit() {
+    
   }
 
   fechaSelected(date){
-    console.log(new Date(date.value).toISOString());
+    this.desde = new Date(date.value).toISOString();
+    let h = new Date(date.value);
+    h.setHours(23,59,59);
+    this.hasta = h.toISOString();
+    this.dateSelected = true;
   }
 
-  getMediciones(desde, hasta){
-    this.medicionService.getMediciones(desde, this.hasta).subscribe( (res: MedicionDTO) => {
-      this.mediciones = res.items;
-      console.log('mediciones ', this.mediciones);
+  dataToGraph(mediciones: Medicion[]): DataLineChart{
+    let values = [];
+    let labels = [];
+    const colors = [{
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,0,0,0.6)',
+    }]
+    const titulo = mediciones[0].tipo;
+    const options = { 
+      responsive: true,
+      scales: {
+        yAxes: [{
+          ticks: {
+            min: 0,
+            max: 100
+          }
+        }]
+      }
+    }
+
+    for (let medicion of mediciones) {
+      values.push(medicion.value)
+      labels.push(medicion.date)        
+    }
+
+    return new DataLineChart([{ data: values, label: titulo}],
+                              labels,
+                              colors,
+                              [],
+                              options)
+  }
+
+  getData(){
+    Swal.fire({
+      allowOutsideClick: false,
+      title: 'Espere porfavor',
+      icon: 'info'
+    })
+    Swal.showLoading();
+    this.medicionService.getMediciones(this.desde, this.hasta, 100, 1, this.selected).subscribe( (res: MedicionDTO) => {
+      Swal.close();
+      console.log('res ', res)
+      if(res.items.length > 0){
+        console.log("llego data")
+        this.graphData = this.dataToGraph(res.items);
+        console.log(this.graphData);
+        this.isData = true;
+      } else {
+        console.log("no llego data")
+        this.isData = false;
+      }      
     });
   }
 }
