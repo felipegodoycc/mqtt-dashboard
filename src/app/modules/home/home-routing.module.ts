fe69f09ae7df2f21f6e256e6bc560bf6f97fe788
Felipe Godoy <felipe.godoy@ceinf.cl>
2020-01-05 01:50:11 -0300

    add nueva ruta a modulo home

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { DispositivosComponent } from './dispositivos/dispositivos.component';
 
 const routes: Routes = [
     { 
@@ -9,6 +10,7 @@ const routes: Routes = [
         children: [
             { path:'dashboard', component: DashboardComponent },
             { path:'formulario', component: FormularioComponent},
+            { path:'dispositivos', component: DispositivosComponent},
             { path: '', pathMatch: 'full', redirectTo: 'dashboard'}
         ]
     },
