e95f7be0d7ae72e1477abba32cd9b087c2376c3d
Felipe Godoy <felipegodoy@workmate.cl>
2020-01-07 11:15:29 -0300

    add guard

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FormularioComponent } from './formulario/formulario.component';
 import { DispositivosComponent } from './dispositivos/dispositivos.component';
+import { AuthGuard } from 'src/app/shared/guards/auth.guard';
 
 const routes: Routes = [
-    { 
+    {
         path: '',
+        canActivate: [AuthGuard],
         children: [
             { path:'dashboard', component: DashboardComponent },
             { path:'formulario', component: FormularioComponent},
