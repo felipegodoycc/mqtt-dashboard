84d13b0aa2aa9b93bfd3e24b0d3a53ed1903d5de
Felipe Godoy <felipegodoy@workmate.cl>
2020-01-07 17:11:56 -0300

    delete getData

--- src/app/modules/home/dashboard/dashboard.component.ts
+++ src/app/modules/home/dashboard/dashboard.component.ts
@@ -16,59 +16,60 @@ export class DashboardComponent implements OnInit {
   mediciones: Medicion[] = [];
   graphData: DataLineChart;
   isData = false;
+
+  dateSelected = false;
+  desde: string;
+  hasta: string;
+
   topics = [
     { value: 'casa/pieza/temp', viewValue: 'casa/pieza/temp'},
     { value: 'casa/pieza/hum', viewValue: 'casa/pieza/hum'},
     { value: 'casa/patio/temp', viewValue: 'casa/patio/temp'},
     { value: 'casa/patio/hum', viewValue: 'casa/patio/hum'},
-  ]
-  selected = this.topics[0].value
-  dateSelected = false;
-  desde: string;
-  hasta: string;
-  page: number = 1;
+  ];
 
-  devices: Device[] = [];
+  selected = this.topics[2].value;
 
   constructor(public medicionService: MedicionService) {
     this.fechaSelected(new Date(Date.now()));
-    this.getData2();
+    this.getData();
+    this.dateSelected = false;
   }
 
   ngOnInit() {
   }
 
-  fechaSelected(date:Date){
-    date.setHours(0,0,0);
+  fechaSelected(date: Date) {
+    date.setHours(0, 0, 0);
     this.desde = new Date(date).toISOString();
     let h = new Date(date);
-    h.setHours(23,59,59);
+    h.setHours(23, 59, 59);
     this.hasta = h.toISOString();
     this.dateSelected = true;
   }
 
-  dataToGraph(mediciones: Medicion[]): DataLineChart{
+  dataToGraph(mediciones: Medicion[]): DataLineChart {
     let values = [];
     let labels = [];
     const colors = [{
       borderColor: 'red',
       backgroundColor: 'rgba(255,0,0,0.6)',
-    }]
+    }];
     const titulo = 'Valores';
     const options = {
       responsive: true,
-    }
+    };
 
     for (let medicion of mediciones) {
-      values.push(medicion.value)
-      labels.push(medicion._id)
+      values.push(medicion.value);
+      labels.push(medicion._id);
     }
 
     return new DataLineChart([{ data: values, label: titulo}],
                               labels,
                               colors,
                               [],
-                              options)
+                              options);
   }
 
   getData(){
@@ -76,40 +77,18 @@ export class DashboardComponent implements OnInit {
       allowOutsideClick: false,
       title: 'Espere porfavor',
       icon: 'info'
-    })
-    Swal.showLoading();
-    this.medicionService.getMediciones(this.desde, this.hasta, 100, this.page, this.selected).subscribe( (res: MedicionDTO) => {
-      Swal.close();
-      console.log('res ', res)
-      if(res.items.length > 0){
-        console.log("llego data")
-        this.graphData = this.dataToGraph(res.items);
-        console.log(this.graphData);
-        this.isData = true;
-      } else {
-        console.log("no llego data")
-        this.isData = false;
-      }
     });
-  }
-
-  getData2(){
-    Swal.fire({
-      allowOutsideClick: false,
-      title: 'Espere porfavor',
-      icon: 'info'
-    })
     Swal.showLoading();
     this.medicionService.getMedicionesPorHora(this.desde, this.hasta, this.selected).subscribe( (res: MedicionDTO) => {
       Swal.close();
-      console.log('res ', res)
-      if(res.items.length > 0){
-        console.log("llego data")
+      console.log('res ', res);
+      if (res.items.length > 0){
+        console.log('llego data');
         this.graphData = this.dataToGraph(res.items);
         console.log(this.graphData);
         this.isData = true;
       } else {
-        console.log("no llego data")
+        console.log('no llego data');
         this.isData = false;
       }
     });
