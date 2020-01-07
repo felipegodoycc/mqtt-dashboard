b57916781bbd4a67ce53c2bdd5982f150b195ed6
Felipe Godoy <felipegodoy@workmate.cl>
2020-01-07 11:57:04 -0300

    add loading a carga de dispositivos

--- src/app/modules/home/dispositivos/dispositivos.component.ts
+++ src/app/modules/home/dispositivos/dispositivos.component.ts
@@ -9,12 +9,14 @@ import { Device } from 'src/app/shared/interface/device.interface';
 })
 export class DispositivosComponent implements OnInit {
   devices: Device[] = [];
+  loading:boolean = true;
 
   constructor(private ewelinkService: EwelinkService) { }
 
   ngOnInit() {
     this.ewelinkService.getDevices().subscribe( (res:any) => {
       this.devices = res.devices;
+      this.loading = false;
       console.log(this.devices)
     })
   }
