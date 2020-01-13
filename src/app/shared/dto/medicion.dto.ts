45aeafc85595be1576ae87f75b9a1ee273bce45d
Felipe Godoy <felipegodoy@workmate.cl>
2020-01-13 12:17:28 -0300

    update dto para mediciones

--- src/app/shared/dto/medicion.dto.ts
+++ src/app/shared/dto/medicion.dto.ts
@@ -1,7 +1,24 @@
 import { Medicion } from '../models/medicion.model';
 
 export class MedicionDTO {
-    ok: Boolean
-    items: Medicion[]
-    total: Number
+    ok: boolean;
+    items: Medicion[];
+    total: number;
+}
+
+export class MedicionesPorHoraDTO {
+  ok: boolean;
+  items: MedicionPorHora[];
+}
+
+export class MedicionPorHora {
+  _id: string;
+  values: Values[];
+  minValue: number;
+  maxValue: number;
+}
+
+class Values {
+  value: number;
+  hora: number;
 }
