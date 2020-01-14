b892e904aafde30780233f29a1cd4768d871ebfb
Felipe Godoy <felipe.godoy@ceinf.cl>
2020-01-13 21:10:38 -0300

    limpieza de componente dashboard

--- src/app/modules/home/dashboard/dashboard.component.ts
+++ src/app/modules/home/dashboard/dashboard.component.ts
@@ -14,14 +14,8 @@ import { BehaviorSubject } from 'rxjs';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  mediciones: Medicion[] = [];
-  graphData: DataLineChart;
-  isData = false;
   refreshCards: BehaviorSubject<boolean> = new BehaviorSubject(false);
-  dateSelected = false;
-  loading = false;
-  desde: string;
-  hasta: string;
+  breakpoint: number;
 
   topics = [
     { value: 'casa/pieza/temp', viewValue: 'Temperatura pieza'},
@@ -30,12 +24,7 @@ export class DashboardComponent implements OnInit {
     { value: 'casa/patio/hum', viewValue: 'Humedad patio'},
   ];
 
-  selected = [this.topics[0].value];
-  breakpoint: number;
-
-  constructor(public medicionService: MedicionService) {
-    this.fechaSelected(new Date(Date.now()));
-    this.dateSelected = false;
+  constructor() {
   }
 
   ngOnInit() {
@@ -44,65 +33,6 @@ export class DashboardComponent implements OnInit {
 
   onResize(event) {
     this.breakpoint = (event.target.innerWidth <= 400) ? 1 : 4;
-  }
-
-  fechaSelected(date: Date) {
-    date.setHours(0, 0, 0);
-    this.desde = new Date(date).toISOString();
-    const h = new Date(date);
-    h.setHours(23, 59, 59);
-    this.hasta = h.toISOString();
-    this.dateSelected = true;
-  }
-
-  dataToGraph(mediciones: MedicionPorHora[]): DataLineChart {
-    const datasets = [];
-    let labels = [];
-    const colors = [
-      { borderColor: 'red' },
-      { borderColor: 'blue' },
-      { borderColor: 'green' },
-      { borderColor: 'black' },
-      { borderColor: 'cyan' }
-    ];
-
-    mediciones.map( (item: MedicionPorHora) => {
-      const preData = {
-        data: item.values.map( val => val.value),
-        label: item._id
-      };
-      labels = item.values.map( val => val.hora);
-      datasets.push(preData);
-    });
-
-    const options = {
-      responsive: true,
-    };
-
-    console.log("Datasets: ", datasets);
-
-    return new DataLineChart(datasets,
-                              labels,
-                              colors,
-                              [],
-                              options);
-  }
-
-  getData() {
-    this.loading = true;
-    this.medicionService.getMedicionesPorHora(this.desde, this.hasta, this.selected.join(',')).subscribe( (res: MedicionesPorHoraDTO) => {
-      console.log('res ', res);
-      if (res.items.length > 0) {
-        console.log('llego data');
-        this.graphData = this.dataToGraph(res.items);
-        console.log(this.graphData);
-        this.isData = true;
-      } else {
-        console.log('no llego data');
-        this.isData = false;
-      }
-      this.loading = false;
-    });
   }
 
   updateCards() {
