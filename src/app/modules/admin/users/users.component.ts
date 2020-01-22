4b0a2c20aab6694bf4c23317b83cbc797c648f0f
Felipe Godoy <felipegodoy@workmate.cl>
2020-01-22 11:20:44 -0300

    add edit y otros

--- src/app/modules/admin/users/users.component.ts
+++ src/app/modules/admin/users/users.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserService } from 'src/app/core/services/user.service';
 import { UsuarioAPI } from 'src/app/shared/models/usuarioAPI.model';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatPaginator } from '@angular/material';
 import { UsersSheetComponent } from './users-sheet/users-sheet.component';
 import { Subscription } from 'rxjs';
+import { UserDeleteDialogComponent } from './user-delete-dialog/user-delete-dialog.component';
 
 @Component({
   selector: 'app-users',
@@ -13,16 +14,17 @@ import { Subscription } from 'rxjs';
 export class UsersComponent implements OnInit {
   loading = true;
   users: UsuarioAPI[];
-  displayedColumns: string[] = ['_id', 'username', 'email', 'role','active'];
+  displayedColumns: string[] = ['_id', 'username', 'email', 'role', 'active', 'options'];
+  totalUsers = 0;
 
   subscription: Subscription = new Subscription();
-
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   constructor(private userService: UserService,
               private dialog: MatDialog) { }
 
   ngOnInit() {
-    this.loadUsers();
+    this.loadUsers(this.paginator.pageIndex + 1, this.paginator.length);
   }
 
   addUsuario(){
@@ -30,15 +32,36 @@ export class UsersComponent implements OnInit {
       this.dialog.open(UsersSheetComponent, { width: '1000px'})
         .afterClosed().subscribe( res => {
           this.loadUsers();
-          console.log("listo");
       })
-    )
-
+    );
   }
 
-  loadUsers(){
-    this.userService.getUsers().subscribe( (res:any) => {
+  editUser(user){
+    this.subscription.add(
+      this.dialog.open(UsersSheetComponent, { width: '1000px', data: user})
+        .afterClosed().subscribe( res => {
+          this.loadUsers(this.paginator.pageIndex + 1,this.paginator.length);
+        })
+    );
+  }
+
+  deleteUser(user){
+    this.subscription.add(
+      this.dialog.open(UserDeleteDialogComponent, { width: '500px'})
+        .afterClosed().subscribe( (result: boolean) => {
+          if ( result === true ) {
+            this.userService.deleteUser(user._id).subscribe( res => {
+              this.loadUsers(this.paginator.pageIndex + 1,this.paginator.length);
+            });
+          }
+        })
+    )
+  }
+
+  loadUsers(page= 1, length = 10){
+    this.userService.getUsers(page, length).subscribe( (res: any) => {
       this.users = res.items;
+      this.totalUsers = res.total;
       this.loading = false;
     });
   }
