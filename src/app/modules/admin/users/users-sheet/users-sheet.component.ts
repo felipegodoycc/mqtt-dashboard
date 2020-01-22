d740c5ed2be43b4a6e1066ab77f745d9b1eda2b9
Felipe Godoy <felipegodoy@workmate.cl>
2020-01-22 11:20:57 -0300

    add edit mode

--- src/app/modules/admin/users/users-sheet/users-sheet.component.ts
+++ src/app/modules/admin/users/users-sheet/users-sheet.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core';
 import { UsuarioAPI } from 'src/app/shared/models/usuarioAPI.model';
 import { NgForm } from '@angular/forms';
 import { UserService } from 'src/app/core/services/user.service';
-import { MatDialogRef } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-users-sheet',
@@ -11,19 +11,30 @@ import { MatDialogRef } from '@angular/material';
 })
 export class UsersSheetComponent implements OnInit {
   usuario: UsuarioAPI = new UsuarioAPI();
+  edit = false;
 
   constructor(private userService: UserService,
-              public dialogRef: MatDialogRef<UsersSheetComponent>) { }
+              public dialogRef: MatDialogRef<UsersSheetComponent>,
+              @Inject(MAT_DIALOG_DATA) public entryData: any) { }
 
   ngOnInit() {
+    if (this.entryData) {
+      this.usuario = this.entryData;
+      this.edit = true;
+    }
   }
 
-  onSubmit(form: NgForm){
-    console.log('form:', this.usuario)
-    this.userService.newUser(this.usuario).subscribe( res => {
-      this.dialogRef.close(true);
-      console.log(res);
-    })
+  onSubmit(form: NgForm) {
+    console.log('form:', this.usuario);
+    if(this.edit){
+      this.userService.editUser(this.usuario).subscribe( res => {
+        this.dialogRef.close(true);
+      });
+    } else {
+      this.userService.newUser(this.usuario).subscribe( res => {
+        this.dialogRef.close(true);
+      });
+    }
   }
 
 }
